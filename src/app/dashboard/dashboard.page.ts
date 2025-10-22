6d1c6a0de610d6566d65c2fb22f87715ee80ac85
ArthurDezan <apdezan78@gmail.com>
2025-10-22 11:16:09 -0300

    first commit

---
+++ src/app/dashboard/dashboard.page.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Api } from '../api';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.page.html',
+  styleUrls: ['./dashboard.page.scss'],
+  standalone: false
+})
+export class DashboardPage implements OnInit {
+
+  constructor(private apiServise: Api) { }
+
+  ngOnInit() {
+    this.carregarDados();
+  }
+
+  carregarDados(): any {
+    this.apiServise.getSensor().subscribe({
+      next: (data: any[]) => {
+        console.log(data);
+      },
+      error: (err) => {
+        console.error('Erro ao carregar dados do sensor:', err);
+      }
+    })
+  }
+
+}
